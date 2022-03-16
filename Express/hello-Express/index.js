d7e3d135a32aa7409b7b8361e7f88cc2b42c5338
tonistorres <sisvenda2011@gmail.com>
2022-03-16 12:56:27 -0300

    feat:Configurando ambiente para o start da aplicação

--- Express/hello-Express/index.js
+++ Express/hello-Express/index.js
@@ -28,6 +28,54 @@ nossa API de forma local, esse método recebe como parâmetro 02(dois) parâmetr
 1- a porta onde ele irá rodar a aplicaão submetida a ele;
 2- uma callback responsável pela execução de todo codigo embutido no seu escopo
  */ 
+
+
+
+const express = require('express');
+const app = express();
+
+/* Rota com caminho '/', utilizando o método GET */
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+/* Rota com caminho '/', utilizando o método POST */
+app.post('/', function (req, res) {
+  res.send('hello world');
+});
+
+/* Rota com caminho '/', utilizando o método PUT */
+app.put('/', function (req, res) {
+  res.send('hello world');
+});
+
+/* Rota com caminho '/', utilizando o método DELETE */
+app.delete('/', function (req, res) {
+  res.send('hello world');
+});
+
+/* Rota com caminho '/' para qualquer método HTTP */
+app.all('/', function (req, res) {
+  res.send('hello world');
+});
+
+/* Ou podemos encadear as requisições para evitar repetir o caminho */
+app
+  .route('/')
+  .get(function (req, res) {
+        // Requisições para rota GET `/` são resolvidas aqui!
+    res.send('hello world get');
+  })
+  .post(function (req, res) {
+        // Requisições para rota POST `/` são resolvidas aqui!
+    res.send('hello world post');
+  });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 }); // 3
