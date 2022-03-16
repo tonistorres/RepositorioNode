a8e17368d782b6bdd98e3ad445c0430d12cbb029
tonistorres <sisvenda2011@gmail.com>
2022-03-15 23:50:23 -0300

    feat:Implementando validação para funcao

--- exercicio-treinando/index.js
+++ exercicio-treinando/index.js
@@ -1,6 +1,11 @@
-// Crie uma função que recebe três parâmetros retorna uma Promise .
+/* 1- Crie uma função que recebe três parâmetros retorna uma Promise .
+   2-Adicionar validação para garantir que todos os valores são numéricos:
+*/
 
 function asinc (paramA, paramB,paramC){
-return new Promise((resolve,reject)=>{})
- 
+return new Promise((resolve,reject)=>{
+    if(paramA!=='number' || paramB!=='number'|| paramC!==number){
+        reject('Informe Números')
+    }
+})
 }
