f590ea2fd24ccf11af50fdb155e1eccada7620ab
tonistorres <sisvenda2011@gmail.com>
2022-03-18 14:04:34 -0300

    feat: Implementando constante que recebe APP_PORT

--- Mazer/index.js
+++ Mazer/index.js
@@ -2,6 +2,13 @@
  * todos seus recurso a uma constante que nomeei de express
 */
 const express = require('express');
+
+// Seguindo Boas Práticas: Criando uma constante 
+// para porta no caso da necessidade de mudar a porta 
+// modificamos em um único local da aplicação e todos
+// demais locais recebem essa alteração automagicamente
+const APP_PORT=3000;
+
 /* agora irei criar uma instancia de express essa instancia
 a partir desse momento irá me da acesso a vários métodos do 
 frameWork Express
@@ -13,4 +20,4 @@ com o método listen do express ela recebe como parametro
 a porta onde esse servidor. É comum passarmos uma callback
 para informar ao usuario que o servidor está rodando 
 */
-app.listen(3000,()=>console.log('Servidor Rodando com Sucesso!!!'));
+app.listen(APP_PORT,()=>console.log(`Servidor Rodando na Porta ${APP_PORT}`));
